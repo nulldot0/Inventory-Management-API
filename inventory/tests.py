91fd5fc06b4a94f66dc29a04a5a893e344184d5c
jong <jhpetalbo@gmail.com>
2020-09-22 23:30:47 +0800

    can create/read product

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase, Client
 from . import models
+from json import dumps
+import random
+
 
 class ModelTest(TestCase):
     def setUp(self):
@@ -16,28 +19,91 @@ class ModelTest(TestCase):
 
 class ViewTest(TestCase):
     def setUp(self):
-        models.Supplier.objects.create(name='test', pk=1)
+        models.Supplier.objects.create(name='supplier1', pk=1)
+        models.Supplier.objects.create(name='supplier2', pk=2)
+
+
 
     def testCreateProduct(self):
         client = Client()
-        product_name = 'test1'
+        product_names = ['test1', 'test2', 'test3', 'test4', 'test5']
+        stocks = [100, 20, 30, 10, 20]
+        suppliers = [1, 2, 1, 2, 1]
+        descriptions = ['description', 'test', '', '', 'test2']
+        data = []
+        [
+            data.append({
+                'name': prod_name,
+                'stock': stock,
+                'supplier': supplier,
+                'description': description,
+            }) for prod_name, stock, supplier, description in zip(product_names, stocks, suppliers, descriptions)
+        ]
+
+        for i, j in zip(data, product_names):
+            response = client.post('/product/create/', {'json_data': dumps(i)})
+            
+            self.assertEqual(response.status_code, 200)
+           
+            product = models.Product.objects.get(name=j)
+ 
+            # prints response
+            print(response.content)
+            # check if product is same as given product_name
+            self.assertEqual(product.name, j)
+
+    def testMassCreateProduct(self):
+        client = Client()
+        product_names = ['test1', 'test2', 'test3', 'test4', 'test5']
+        stocks = [100, 20, 30, 10, 20]
+        suppliers = [1, 2, 1, 2, 1]
+        descriptions = ['description', 'test', '', '', 'test2']
+        data = []
+        # 
+        [
+            data.append({
+                'name': prod_name,
+                'stock': stock,
+                'supplier': supplier,
+                'description': description,
+            }) for prod_name, stock, supplier, description in zip(product_names, stocks, suppliers, descriptions)
+        ]
+
+        response = client.post('/product/create/', {'json_data': dumps(data), 'isMass': True})
+        self.assertEqual(len(models.Product.objects.all()), 5)
+
+        print(response.content)
+
+    def testReadProduct(self):
+        pks = [x for x in range(1, 10)]
+        for i in pks:
+            models.Product.objects.create(
+                pk=i,
+                name=f'Product {i}', 
+                stock=random.randint(1, 100),
+                description=f'description {i}',
+                supplier=models.Supplier.objects.get(pk=random.choice([1,2]))
+            )
+
+        client = Client()
 
         data = {
-            'name': product_name,
-            'stock': 100,
-            'description': 'epic',
-            'supplier': 1
+            'productId': random.choice(pks)
         }
-        from json import dumps
-        response = client.post('/product/create/', {'json_data': dumps(data)})
-        
-        self.assertEqual(response.status_code, 200)
-        product = models.Product.objects.get(name=product_name)
-        
-        # prints response
-        print(response.content)
-        # check if product is same as given product_name
-        self.assertEqual(product.name, product_name)
+        res = client.get('/product/read/', {
+                'json_data': dumps(data)
+            })
 
-    def massProductCreate(self):
-        pass
+        # for mass read
+        data = {
+            'productIds': pks
+        }
+        res = client.get('/product/read/', {
+                'json_data': dumps(data), 'isMass': True
+            })
+        
+        print(res.content)
+
+        self.assertEqual(res.status_code, 200)
+
+        print(res.content)
--- inventory/views.py
+++ inventory/views.py
@@ -1,43 +1,73 @@
 from django.shortcuts import render, HttpResponse
 from .models import *
 import json
-def product_action(request, action):
-    if request.method != 'POST':        
-        return HttpResponse('not a POST request.')
-    
-    if action == 'create':
-        data = request.POST.get('json_data')
-        data = json.loads(data)
 
-        # filters the supplier if it exist or it is provided
-        try:
-            if data['supplier']:
-                if Supplier.objects.filter(pk=data['supplier']).exists():
-                    data['supplier'] = Supplier.objects.get(pk=data['supplier'])
-                    supplier = data['supplier'].name
-                else:
-                    data['supplier'] = None
-                    supplier = 'No supplier found'
-        except:
-            supplier = None
+def create_product(data):
+    '''This function takes a data with dict as it's type'''
 
+    # checks if a supplier is provided in the data given
+    if data.get('supplier'):
+        data['supplier'] = Supplier.objects.get(pk=data['supplier'])
+        product = Product(**data)   
+        data['supplier'] = data.get('supplier').serializable_value('name')
+    else:
         product = Product(**data)
-       
-        product.save()
 
-        response_details = {
-                'product_name': product.name,
-                'stock': product.stock,
-                'supplier': supplier,
-                'description': product.description
-            }
+    product.save()
 
-        return HttpResponse(f'{json.dumps(response_details)}')
+    # returns the details of product in dict type
+    return {
+        'product_name': product.name,
+        'stock': product.stock,
+        'supplier': data.get('supplier'),
+        'description': data.get('description')
+    }
 
-    elif action == 'read':
-        pass
-    elif action == 'update':
-        pass
-    elif action == 'delete':
-        pass
+def mass_create_product(datas):
+    return [ create_product(data) for data in datas ]
+
+def read_product(pk):
+    product = Product.objects.get(pk=pk)
+    
+    if product.supplier:
+        supplier = product.supplier.serializable_value('name')
+    else:
+        supplier = None
+
+    return {
+        'name': product.name,
+        'stock': product.stock,
+        'description': product.description,
+        'supplier': supplier,
+    }
+
+def mass_read_product(pks):
+    return [ read_product(pk) for pk in pks ]
+
+def product_action(request, action):
+    if request.method == 'GET':        
+        if action == 'read':
+            loaded_data = json.loads(request.GET.get('json_data'))
+
+            if request.GET.get('isMass'):
+                response_data = mass_read_product(loaded_data['productIds'])
+            else:
+                response_data = read_product(loaded_data['productId'])
+
+            return HttpResponse(json.dumps(response_data))
+    else:
+        loaded_data = json.loads(request.POST.get('json_data'))
+
+        if action == 'create':
+            if request.POST.get('isMass'):
+                response_data = mass_create_product(loaded_data)
+            else:
+                response_data = create_product(loaded_data)
+            
+            return HttpResponse(json.dumps(response_data))
+
+        elif action == 'update':
+            pass
+        elif action == 'delete':
+            pass
    
